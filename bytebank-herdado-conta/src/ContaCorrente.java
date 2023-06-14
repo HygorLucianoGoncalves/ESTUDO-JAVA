996a43e7330460b1feb3cc531a8a08cfd881d22b
Hygor Luciano <128493484+HygorLucianoGoncalves@users.noreply.github.com>
2023-06-14 13:56:09 -0300

    Create contas cc e cp

---
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,7 @@
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(int agencia, int numero) { //Contrudore não são hedados da class mãe 
+		super(agencia, numero);
+	}
+}
---
+++ bytebank-herdado-conta/src/ContaPoupanca.java
@@ -0,0 +1,7 @@
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(int agencia, int numero) {//Contrudore não são hedados da class mãe 
+		super(agencia, numero);
+	}
+}
