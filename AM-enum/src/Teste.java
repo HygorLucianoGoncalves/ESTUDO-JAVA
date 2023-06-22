f4cc8c4d4ae612b9de70cf189cb819d058cc9cf0
Hygor Luciano <128493484+HygorLucianoGoncalves@users.noreply.github.com>
2023-06-22 17:20:51 -0300

    usando try e catch

--- AM-enum/src/Teste.java
+++ AM-enum/src/Teste.java
@@ -3,9 +3,15 @@
 public class Teste {
 
 	public static void main(String[] args) {
+	
+		Prioridade pMin = Prioridade.MIN;
+		Prioridade pMax = Prioridade.MAX;
 		
-		Thread t = new Thread()
- 
+		System.out.println(pMin.name());
+		System.out.println(pMax.name());
+		
+		System.out.println(pMin.ordinal());
+		System.out.println(pMax.ordinal());
 	}
 
 }
